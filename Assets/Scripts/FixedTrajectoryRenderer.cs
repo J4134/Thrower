22dcc7e44a5731abf1c3b22a3b6da49189acb405
JABA <55496988+J4134@users.noreply.github.com>
2020-08-10 19:12:18 +0300

    Полностью переписал TrajectoryRenderer

---
+++ Assets/Scripts/FixedTrajectoryRenderer.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FixedTrajectoryRenderer : TrajectoryRenderer
+{
+    [SerializeField] private int _dotsCount = 10;
+    [SerializeField] private GameObject dotPrefab;
+
+    private void Awake()
+    {
+         _gravity = Physics2D.gravity;
+    }
+
+    private void Start()
+    {
+        CreateDots(_dotsCount);
+    }
+
+    private void CreateDots(int dotsCount)
+    {
+        for (int i = 0; i < dotsCount; i++)
+        {
+            GameObject newDot = Instantiate(dotPrefab, gameObject.transform);
+            newDot.SetActive(false);
+            _instantiatedDots.Add(newDot);
+            _instantiatedDotsPosition.Add(newDot.transform);
+        }
+    }
+
+    public override void DrawTrajectory(Vector2 origin, Vector2 throwVector)
+    {
+        if (_previousThrowVector != throwVector)
+        {
+            RelocateDots(_instantiatedDotsPosition, CalculateDotsPositions(origin, throwVector, _dotsCount, 0.1f));
+
+            foreach (GameObject dot in _instantiatedDots)
+            {
+                dot.SetActive(true);
+            }
+        }
+    }
+
+    public override void DeleteTrajectory()
+    {
+        if (_instantiatedDots.Count > 0)
+        {
+            foreach (GameObject dot in _instantiatedDots)
+            {
+                dot.SetActive(false);
+            }
+        }
+    }
+
+
+}
--- Assets/Scripts/TrajectoryRenderer.cs
+++ Assets/Scripts/TrajectoryRenderer.cs
@@ -1,103 +1,29 @@
 ﻿using System.Collections.Generic;
 using UnityEngine;
 
-public class TrajectoryRenderer : MonoBehaviour
+public abstract class TrajectoryRenderer : MonoBehaviour
 {
 
     #region Field Declarations
 
-    [SerializeField] private GameObject dotPrefab;
-
-    [SerializeField] private DotsAmount _dotsAmount = DotsAmount.Fixed;
-    [SerializeField] private int _dotsCount = 10;
-
-
-
-    private enum DotsAmount { Fixed, Dynamic };
-    
-    private List<GameObject> _trajectoryDots = new List<GameObject>() { };
-    private List<Transform> _trajectoryDotsPositins = new List<Transform>() { };
-
-    private Vector3[] _dotsPositions;
-    private Vector2 _previousThrowVector;
-
-    private Vector2 _gravity;
-
-    private void Start()
-    {
-        _gravity = Physics2D.gravity;
-        CreateDots(_dotsAmount);
-    }
+    private protected List<GameObject> _instantiatedDots = new List<GameObject>() { };
+    private protected List<Transform> _instantiatedDotsPosition = new List<Transform>() { };
+    private protected Vector2 _previousThrowVector;
+    private protected Vector2 _gravity;
 
     #endregion
 
     #region Controllers
 
-    public void DrawTrajectory(Vector2 origin, Vector2 throwVector)
-    {
-        if (_previousThrowVector != throwVector)
-        {
-            if (_dotsAmount == DotsAmount.Fixed)
-            {
-                RelocateDots(_trajectoryDotsPositins, CalculateDotsPositions(origin, throwVector, _dotsCount, 0.1f));
+    public abstract void DrawTrajectory(Vector2 origin, Vector2 throwVector);
 
-                foreach (GameObject dot in _trajectoryDots)
-                {
-                    dot.SetActive(true);
-                }
-            }
-            else if (_dotsAmount == DotsAmount.Dynamic)
-            {
-
-            }
-        }
-    }
-
-    public void DeleteTrajectory()
-    {
-        if (_trajectoryDots.Count > 0)
-        {
-            if (_dotsAmount == DotsAmount.Dynamic)
-            {
-                // TO DO: переписать согласно новой логике.
-                foreach (GameObject dot in _trajectoryDots)
-                {
-                    Destroy(dot);
-                }
-
-            }
-            else if (_dotsAmount == DotsAmount.Fixed)
-            {
-                foreach (GameObject dot in _trajectoryDots)
-                {
-                    dot.SetActive(false);
-                }
-            }
-        }
-
-        // Нужно как-то очистить previousThrowVector, чтобы после броска траектория строилась заново, даже при совпадающих векторах.
-        _previousThrowVector = new Vector2(0f, 0f);
-    }
-
-    private void CreateDots(DotsAmount dotsAmount)
-    {
-        if (dotsAmount == DotsAmount.Fixed)
-        {
-            for (int i = 0; i < _dotsCount; i++)
-            {
-                GameObject newDot = Instantiate(dotPrefab, gameObject.transform);
-                newDot.SetActive(false);
-                _trajectoryDots.Add(newDot);
-                _trajectoryDotsPositins.Add(newDot.transform);
-            }
-        }
-    }
+    public abstract void DeleteTrajectory();
 
     #endregion
 
     #region Calculations
 
-    private Vector3[] CalculateDotsPositions(Vector2 origin, Vector2 throwVector, int dotsCount, float timeStep) 
+    private protected Vector3[] CalculateDotsPositions(Vector2 origin, Vector2 throwVector, int dotsCount, float timeStep) 
     {
         Vector3[] dotsPositions = new Vector3[dotsCount];
 
@@ -111,7 +37,7 @@ public class TrajectoryRenderer : MonoBehaviour
         return dotsPositions;
     }
 
-    private void RelocateDots(List<Transform> instantiatedDots, Vector3[] dotsPositions)
+    private protected void RelocateDots(List<Transform> instantiatedDots, Vector3[] dotsPositions)
     {
         for (int i = 0; i < instantiatedDots.Count; i++)
         {
